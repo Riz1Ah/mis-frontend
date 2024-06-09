3be8214d5dfa221407bb39349d80f598c905854d
Rizwan <fbriz007@gmail.com>
2024-06-09 14:43:25 +0530

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,46 +6,64 @@ import Message from "./components/Message";
 import produce from "immer";
 
 function App() {
-  const handleOnSelect = (item:string) => console.log(item)
+  const handleOnSelect = (item: string) => console.log(item);
   // return ( <ListGroup items={["London", "Paris", "NY", "LA", "San Fransisco"]} heading={"Cities"} onSelectItem={handleOnSelect}/> );
 
-  const [alertState, setAlertState] = useState(0)
+  const [alertState, setAlertState] = useState(0);
   const onClickHandler = () => {
-    setAlertState(1)
-  }
+    setAlertState(1);
+  };
   const onClickCloseHandler = () => {
-    setAlertState(0)
-  }
+    setAlertState(0);
+  };
 
   const [immerVar, setImmerVar] = useState([
-    {id:1, bug:false},
-    {id:2, bug:false}
-  ])
+    { id: 1, bug: false },
+    { id: 2, bug: false },
+  ]);
 
   const showImmerOutput = () => {
-    const item = immerVar.find(item => item.id===1)
-    console.log(item ? item.bug:null)
-  }
+    const item = immerVar.find((item) => item.id === 1);
+    console.log(item ? item.bug : null);
+  };
 
   const useImmer = () => {
     // setImmerVar(immerVar.map(item => item.id===1? {...item, bug:true}: item))
-    setImmerVar(produce(draft => {
-      const iv = draft.find(iv => iv.id===1)
-      if(iv) iv.bug=true
-    }))
-  }
+    setImmerVar(
+      produce((draft) => {
+        const iv = draft.find((iv) => iv.id === 1);
+        if (iv) iv.bug = true;
+      })
+    );
+  };
 
   return (
     <>
-    {alertState===1 && <Alert children="What Have You Done!!" onClickClose={onClickCloseHandler}/>}
-  <Button children="Dont Click Here" color={"primary"} onclickHandle={onClickHandler} />
-  <ListGroup items={["London", "Paris", "NY", "LA", "San Fransisco"]} heading={"Cities"} onSelectItem={handleOnSelect}/> 
+      {alertState === 1 && (
+        <Alert
+          children="What Have You Done!!"
+          onClickClose={onClickCloseHandler}
+        />
+      )}
+      <Button
+        children="Dont Click Here"
+        color={"primary"}
+        onclickHandle={onClickHandler}
+      />
+      <ListGroup
+        items={["London", "Paris", "NY", "LA", "San Fransisco"]}
+        heading={"Cities"}
+        onSelectItem={handleOnSelect}
+      />
 
-    <button onClick={useImmer}> ImmerUsage</button>
-    {immerVar.map(iv=> <p key={iv.id}>{iv.id} {iv.bug? 'Fixed': 'Bug'}</p>)}
-  </>
-)
-
+      <button onClick={useImmer}> ImmerUsage</button>
+      {immerVar.map((iv) => (
+        <p key={iv.id}>
+          {iv.id} {iv.bug ? "Fixed" : "Bug"}
+        </p>
+      ))}
+    </>
+  );
 }
 
 export default App;
